33c7a876a7a747431bbe84fde746dac031d1fb49
Kelvin Luck <git@kelvinluck.com>
2013-02-22 17:09:43 +0000

    Added grunt-contrib-watch and an example configuration to automatically re-generate the site when you change the sources.

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,13 +34,30 @@ module.exports = function(grunt) {
     },
 		haggerston: {
 
-		}
+		},
+    watch: {
+      content: {
+        files: [
+          'src/content/**/*.json',
+          'src/content/**/*.md',
+          'src/templates/**/*.html'
+        ],
+        tasks: 'build'
+      },
+      styles: {
+        files: [
+          'src/styles/**/*.stylus'
+        ],
+        tasks: 'stylus'
+      }
+    }
 
 	});
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-haggerston');
 
 	grunt.registerTask('build', ['clean', 'copy', 'stylus', 'haggerston']);
